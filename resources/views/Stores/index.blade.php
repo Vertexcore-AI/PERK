d10e48baed9fccc52ec8ebf8e6a409457127ba26
sanjana <sanjanarathnayake692@gmail.com>
2025-09-15 18:08:44 +0530

    fix formatting issues of each files

--- resources/views/Stores/index.blade.php
+++ resources/views/Stores/index.blade.php
@@ -37,21 +37,34 @@
 <div class="card stretch stretch-full">
     <div class="card-header">
         <h5 class="card-title">Stores</h5>
-        <div class="dropdown">
-            <a href="javascript:void(0);" class="avatar-text avatar-sm" data-bs-toggle="dropdown"
-                data-bs-offset="25, 25">
-                <div data-bs-toggle="tooltip" title="Options">
-                    <i data-lucide="more-vertical"></i>
+        <!-- <div class="card-header-action">
+                <div class="card-header-btn">
+                    <div data-bs-toggle="tooltip" title="Delete">
+                        <a href="javascript:void(0);" class="avatar-text avatar-xs bg-danger" data-bs-toggle="remove"> </a>
+                    </div>
+                    <div data-bs-toggle="tooltip" title="Refresh">
+                        <a href="javascript:void(0);" class="avatar-text avatar-xs bg-warning" data-bs-toggle="refresh"> </a>
+                    </div>
+                    <div data-bs-toggle="tooltip" title="Maximize/Minimize">
+                        <a href="javascript:void(0);" class="avatar-text avatar-xs bg-success" data-bs-toggle="expand"> </a>
+                    </div>
                 </div>
-            </a>
-            <div class="dropdown-menu dropdown-menu-end">
-                <a href="{{ route('stores.create') }}" class="dropdown-item"><i data-lucide="plus"></i>New Store</a>
-                <a href="javascript:void(0);" class="dropdown-item"><i data-lucide="download"></i>Export</a>
-                <a href="javascript:void(0);" class="dropdown-item"><i data-lucide="filter"></i>Filter</a>
-            </div>
-        </div>
+                <div class="dropdown">
+                    <a href="javascript:void(0);" class="avatar-text avatar-sm" data-bs-toggle="dropdown" data-bs-offset="25, 25">
+                        <div data-bs-toggle="tooltip" title="Options">
+                            <i data-lucide="more-vertical"></i>
+                        </div>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-end">
+                        <a href="{{ route('stores.create') }}" class="dropdown-item"><i data-lucide="plus"></i>New Vendor</a>
+                        <a href="javascript:void(0);" class="dropdown-item"><i data-lucide="download"></i>Export</a>
+                        <a href="javascript:void(0);" class="dropdown-item"><i data-lucide="filter"></i>Filter</a>
+                        <div class="dropdown-divider"></div>
+                        <a href="javascript:void(0);" class="dropdown-item"><i data-lucide="settings"></i>Settings</a>
+                    </div>
+                </div>
+            </div> -->
     </div>
-
     <div class="card-body custom-card-action p-0">
         <div class="table-responsive">
             <table class="table table-hover mb-0">
